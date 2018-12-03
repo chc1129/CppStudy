5c8959a7e6dfd2bf915b4954ed390a8110e677d2
chc1129 <chc1129@gmail.com>
2018-12-03 23:58:05 +0900

    update

---
+++ 24/test3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Neko;
+
+class Kitty {
+  char *str;
+public:
+  friend Neko;
+  Kitty(char *str) { Kitty::str = str; }
+} obj("Kitty on your lap");
+
+class Neko {
+public:
+  Neko(Kitty &obj) {
+    cout << obj.str << '\n';
+  }
+};
+
+int main() {
+  Neko obj(::obj);
+  return 0;
+}
