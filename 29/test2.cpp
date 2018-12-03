f50ebbfe678142dd5cccf00fb4cbc99c046a5e7c
chc1129 <chc1129@gmail.com>
2018-12-04 02:25:58 +0900

    update

---
+++ 29/test2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Kitty {
+public:
+  Kitty( char *str) {
+    cout << str;
+  }
+};
+
+int main() {
+  Kitty *obj;
+  obj = new Kitty("Kitty on your lap\n");
+
+  delete obj;
+  return 0;
+}
